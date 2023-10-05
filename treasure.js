afe58b574ed2264eebd97fb6dfb0ee00c8141e49
pc1 <pest.suzukick@gmail.com>
2023-10-06 00:22:53 +0900

    a

--- treasure.js
+++ treasure.js
@@ -22,23 +22,51 @@ document.addEventListener('DOMContentLoaded', function () {
     for (let i = 1; i <= 12; i++) {
       const display_id = 'display_input' + i;
       document.getElementById(display_id).innerHTML = 'モンスター名もしくは図鑑No.を入力';
-      resultArray.push({ num: i, name: '', size: '', rare: '' })
+      resultArray.push({ num: i })
     }
   })();
 });
 
 function input(elem) {
   const index = matchID(elem.value);
-  const result = result(index);
   const display_id = 'display_' + elem.id;
-  document.getElementById(display_id).innerHTML = result;
+  document.getElementById(display_id).innerHTML = index[1];
   document.getElementById('display_test').innerHTML = index[0];
-  console.log(1)
+  console.log(resultArray[11].num)
 }
 
-function result(index) {
-  if (index[0] > -1) return csvArray[index[0]][index[1]];
-  return '該当なし';
+function reset() {
+  const container = document.getElementById('table_result');
+  if (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+function result() {
+  for (let i = 0; i < 12; i++) {
+    resultArray[i].name = csvArray[i + 2][1]
+    resultArray[i].size = csvArray[i + 2][3]
+    resultArray[i].rare = csvArray[i + 2][4]
+  }
+  resultArray.sort((a, b) => a.rare - b.rare);
+  for (let i = 0; i < 12; i++) {
+    console.log(resultArray[i])
+  }
+
+  const container = document.getElementById('table_result');
+  if (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  const table = document.createElement('table');
+  container.appendChild(table);
+  const array = ['name', 'size', 'rare']
+  for (const i of resultArray) {
+    let newRow = table.insertRow();
+    for (const p of array) {
+      let cell = newRow.insertCell();
+      cell.innerHTML = i[p];
+    }
+  }
 }
 
 function test() {
