75da0b060eee0b68d1517f38de7667efec62d5f9
Pestong <78185764+pestong@users.noreply.github.com>
2023-10-10 09:14:53 +0900

    a

--- treasure.js
+++ treasure.js
@@ -38,16 +38,6 @@ document.addEventListener('DOMContentLoaded', function () {
   })();
 });
 
-const textarea = document.getElementById('myTextarea');
-
-textarea.addEventListener('input', function () {
-    const inputValue = textarea.value;
-
-    const outputElement = document.getElementById('output');
-
-    outputElement.textContent = '入力内容: ' + inputValue;
-});
-
 function input(elem) {
   const display = document.getElementById('display_' + elem.id);
   const num = parseInt(elem.id.replace('input', ''));
