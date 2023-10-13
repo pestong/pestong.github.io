278eecb03e8e637365ec952eb54285b468acd3fa
Pestong <78185764+pestong@users.noreply.github.com>
2023-10-13 12:35:15 +0900

    a

--- treasure.js
+++ treasure.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function () {
   for (let i = 0; i < 12; i++) {
     display.push('未入力');
   }
-  document.getElementById('display_input').innerHTML  = display.join('<br>')
+  document.getElementById('display_input').innerHTML = display.join('<br>')
 });
 
 window.onload = function () {
@@ -69,7 +69,7 @@ function input(elem) {
       display.push('未入力');
     }
   }
-  document.getElementById('display_input').innerHTML  = display.join('<br>')
+  document.getElementById('display_input').innerHTML = display.join('<br>')
 }
 
 function matchID(input) {
@@ -90,12 +90,20 @@ function matchID(input) {
 }
 
 function reset() {
+  const mapname = document.getElementById('input_mapname')
+  mapname.value = ''
   const textarea = document.getElementById('textarea_input')
   textarea.value = ''
   input(textarea);
   const container = document.getElementById('table_result');
   if (container.hasChildNodes()) {
     container.removeChild(container.firstChild);
+  }
+  const container_score = document.getElementById('container_score');
+  if (container_score.hasChildNodes()) {
+    while (container_score.firstChild) {
+      container_score.removeChild(container_score.firstChild);
+    }
   }
 }
 
@@ -106,12 +114,19 @@ function result() {
     resultArray.push({
       name: csvArray[array][1],
       size: csvArray[array][3],
-      rare: csvArray[array][4],
+      rare: parseInt(csvArray[array][4]),
       rareName: rareArray[csvArray[array][4] - 1],
       etcName: etcArray[csvArray[array][5]]
     });
   }
   resultArray.sort((a, b) => a.rare - b.rare);
+
+  const score = check(resultArray);
+  const text = label(score[0])
+  let mob = ''
+  if (score[1] !== -1) {
+    mob = score[1][0].name
+  }
 
   const container = document.getElementById('table_result');
   if (container.hasChildNodes()) {
@@ -120,26 +135,129 @@ function result() {
   const table = document.createElement('table');
   container.appendChild(table);
 
-  let newRow = table.insertRow();
-  for (const p of tableArray1) {
-    let cell = newRow.insertCell();
-    cell.innerHTML = p;
-  }
-
-  for (const i of resultArray) {
+  if (resultArray.length > 0) {
     let newRow = table.insertRow();
-    newRow.style.backgroundColor = colorArray[i.rare-1];
-    for (const p of tableArray2) {
+    for (const p of tableArray1) {
       let cell = newRow.insertCell();
-      // if(i[p]==='ドラキー'){
-      //   cell.style.color = '#ff0000';
-      //   cell.style.fontWeight = "bold";
-      // }
-      cell.innerHTML = i[p];
+      cell.innerHTML = p
+    }
+
+    for (const i of resultArray) {
+      let newRow = table.insertRow();
+      newRow.style.backgroundColor = colorArray[i.rare - 1];
+      for (const p of tableArray2) {
+        let cell = newRow.insertCell();
+        if (i[p] === mob) {
+          cell.style.color = '#ff0000'
+          cell.style.fontWeight = 'bold'
+        }
+        cell.innerHTML = i[p]
+      }
     }
   }
+
+  const container_score = document.getElementById('container_score');
+  if (container_score.hasChildNodes()) {
+    while (container_score.firstChild) {
+      container_score.removeChild(container_score.firstChild);
+    }
+  }
+  const div1 = document.createElement('div');
+  container_score.appendChild(div1);
+  div1.style.fontWeight = 'bold'
+  div1.style.textDecoration = "underline"
+  div1.innerHTML = document.getElementById('input_mapname').value
+
+  const div2 = document.createElement('div');
+  const div2text = document.createElement('div');
+  const div2mob = document.createElement('span');
+  div2mob.textContent = mob
+  div2mob.style.color = '#ff0000'
+  div2mob.style.fontWeight = 'bold'
+  div2text.appendChild(div2mob);
+  div2text.appendChild(document.createTextNode(text));
+  div2.appendChild(div2text);
+  container_score.appendChild(div2);
 }
 
-function score() {
+function check(result) {
+  let score = [0, -1]
+  const lengthTest = [4, 3, 2, 1, 1, 1]
+  const length = []
+  for (let i = 1; i <= 6; i++) {
+    length.push(result.filter(e => e.rare === i).length);
+  }
+  const size = [
+    result.filter(e => e.rare <= 2).filter(e => e.size === '1.0倍'),
+    result.filter(e => e.rare === 3).filter(e => e.size === '1.0倍'),
+    result.filter(e => e.rare >= 4).filter(e => e.size === '1.0倍'),
+    result.filter(e => e.rare === 1).filter(e => e.size === '1.2倍')
+  ]
 
+  if (JSON.stringify(lengthTest) === JSON.stringify(length)) {
+    if (size[0].length > 1) {
+      score[0] = 1
+    }
+    if (size[0].length === 1) {
+      if (size[3].length === 0) {
+        score[0] = 2
+        score[1] = size[0]
+      }
+      if (size[3].length === 1) {
+        score[0] = 3
+        score[1] = size[0]
+      }
+      if (size[3].length > 1) {
+        score[0] = 4
+        score[1] = size[0]
+      }
+    }
+    if (size[0].length === 0) {
+      if (size[1].length > 1) {
+        score[0] = 1
+      }
+      if (size[1].length === 1) {
+        if (size[3].length === 0) {
+          score[0] = 2
+          score[1] = size[1]
+        }
+        if (size[3].length === 1) {
+          score[0] = 3
+          score[1] = size[1]
+        }
+        if (size[3].length > 1) {
+          score[0] = 4
+          score[1] = size[1]
+        }
+      }
+      if (size[1].length === 0) {
+        if (size[2].length > 1) {
+          score[0] = 6
+        }
+        if (size[2].length === 1) {
+          score[0] = 5
+          score[1] = size[2]
+        }
+        if (size[2].length === 0) {
+          score[0] = 6
+        }
+      }
+    }
+  } else {
+    return score
+  }
+  return score
+}
+
+function label(result) {
+  const scoreArray = [
+    `判定不可。入力を確認してください`,
+    `大量発生の特徴はありません`,
+    `が大量発生`,
+    `が大量発生（クランベリー型）`,
+    `が大量発生 ただし４匹編成が出現しにくい`,
+    `のみ等倍 大量発生するかは要検証`,
+    `４匹編成が出現しにくく、レベリング不向きな構成です`
+  ]
+  return scoreArray[result]
 }
